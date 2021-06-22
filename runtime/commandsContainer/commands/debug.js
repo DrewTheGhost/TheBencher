065c7ceb79b8b88c8855094f5466ab028fe8cdd4
Drew <thatonedrew22@gmail.com>
2021-06-22 10:25:32 -0500

    Add message event latency

--- runtime/commandsContainer/commands/debug.js
+++ runtime/commandsContainer/commands/debug.js
@@ -6,13 +6,16 @@ module.exports = {
     description: "Provides the bot's current latency.",
     controlled: false,
     async fn(message, suffix, bot) {
-        let response = []
-        let uptimeSeconds = Math.trunc(bot.uptime/1000),
+        let event = Date.now(),
+            response = [],
+            uptimeSeconds = Math.trunc(bot.uptime/1000),
             uptimeMinutes = Math.trunc(uptimeSeconds/60),
             uptimeHours = Math.trunc(uptimeMinutes/60),
             uptimeDays = Math.trunc(uptimeHours/24)
+
         response.push("```js")
-        response.push(`WebSocket latency: ${bot.ws.ping}ms`)
+        response.push(`WebSocket latency: ${bot.ws.ping} ms`)
+        response.push(`Message Event Latency: ${event - message.createdTimestamp} ms`)
         response.push(`Uptime: ${uptimeDays}d:${uptimeHours % 24}h:${uptimeMinutes % 60}m:${uptimeSeconds % 60}s`)
         response.push(`Command modules: [${await bot.commands.map(m => `"${m.name}"`)}]`)
         response.push(`Client Options: ${util.inspect(bot.options, {depth: 1})}`)
