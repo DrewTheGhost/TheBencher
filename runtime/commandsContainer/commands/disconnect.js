d5facedbb13168e8e4fd454cad1edbb45c4fe2fc
Drew <thatonedrew22@gmail.com>
2021-09-17 17:39:37 -0500

    Disconnect needs to drop queue

--- runtime/commandsContainer/commands/disconnect.js
+++ runtime/commandsContainer/commands/disconnect.js
@@ -4,7 +4,7 @@ module.exports = {
     aliases: [],
     description: "Disconnects the bot",
     controlled: false,
-    fn(message) {
+    fn(message, _suffix, _bot, db) {
         const connection = getVoiceConnection(message.guild.id)
         if(connection == undefined) {
             return message.channel.send("Errr.. Disconnect.. From where??")
@@ -14,5 +14,11 @@ module.exports = {
         }
         connection.destroy()
         message.channel.send("Okay bye :)!")
+        db.client.query("DELETE FROM queue;", function(err, res) {
+            if(err) {
+                console.error(err)
+            }
+            console.log(`Dropping queue from disconnect command, rowCount: ${res.rowCount}`)
+        })
     }
 } 
