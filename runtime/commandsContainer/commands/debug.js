10ac3c350aae1b416d30511792a201ec23af0152
Drew <thatonedrew22@gmail.com>
2021-09-18 21:20:08 -0500

    Convert Debug to Embed

--- runtime/commandsContainer/commands/debug.js
+++ runtime/commandsContainer/commands/debug.js
@@ -5,21 +5,46 @@ module.exports = {
     aliases: ["ping"],
     description: "Provides the bot's current latency.",
     controlled: false,
-    async fn(message, suffix, bot) {
+    async fn(message, _suffix, bot) {
         let event = Date.now(),
-            response = [],
             uptimeSeconds = Math.trunc(bot.uptime/1000),
             uptimeMinutes = Math.trunc(uptimeSeconds/60),
             uptimeHours = Math.trunc(uptimeMinutes/60),
-            uptimeDays = Math.trunc(uptimeHours/24)
-
-        response.push("```js")
-        response.push(`WebSocket latency: ${bot.ws.ping} ms`)
-        response.push(`Message Event Latency: ${event - message.createdTimestamp} ms`)
-        response.push(`Uptime: ${uptimeDays}d:${uptimeHours % 24}h:${uptimeMinutes % 60}m:${uptimeSeconds % 60}s`)
-        response.push(`Command modules: [${await bot.commands.map(m => `"${m.name}"`)}]`)
-        response.push(`Client Options: ${util.inspect(bot.options, {depth: 1})}`)
-        response.push("```")
-        message.channel.send(response.join("\n\n"))
+            uptimeDays = Math.trunc(uptimeHours/24),
+            fields = []
+            fields.push({
+                "name": "WebSocket latency",
+                "value": `\`${bot.ws.ping} ms\``,
+                "inline": true
+            })
+            fields.push({
+                "name": "Message Event Latency",
+                "value": `\`${message.createdTimestamp - event} ms\``,
+                "inline": true
+            })
+            fields.push({
+                "name": "Uptime",
+                "value": `${uptimeDays}d:${uptimeHours % 24}h:${uptimeMinutes % 60}m:${uptimeSeconds % 60}s`,
+                "inline": false
+            })
+            fields.push({
+                "name": "Command Modules",
+                "value": `${Array.from(await bot.commands.keys()).join(", ")}`,
+                "inline": false
+            })
+            fields.push({
+                "name": "Client Options",
+                "value": `\`\`\`js\n${util.inspect(bot.options, {depth: 1})}\n\`\`\``,
+                "inline": false
+            })
+        let debug = [
+            {
+                "content": null,
+                "title": "The Bencher Debug",
+                "color": 5814783,
+                "fields": fields
+            }
+        ]
+        message.channel.send({embeds: debug})
     }
 }
