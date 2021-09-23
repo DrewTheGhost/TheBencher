d9be826f56eebee2b26100b8e2ee2bcf51a095c0
Drew <thatonedrew22@gmail.com>
2021-09-23 07:46:56 -0500

    Add Pause Command

--- runtime/commandsContainer/commands/pause.js
+++ runtime/commandsContainer/commands/pause.js
@@ -1,11 +1,20 @@
-const { getVoiceConnection, createAudioResource } = require("@discordjs/voice")
+const { AudioPlayerStatus } = require("@discordjs/voice")
+let player = require("./play.js").player
 
 module.exports = {
     name: "pause",
     aliases: ["stop"],
     description: "Pauses the current song.",
     controlled: false,
-    fn(message, _suffix, bot, db) {
-        // TODO
+    fn(message) {
+        if(player.state.status == AudioPlayerStatus.Paused) {
+            player.unpause()
+            message.channel.send("Startin' the phat beats back up, you know this shit all natty.")
+        } else if(player.state.status == (AudioPlayerStatus.Idle || AudioPlayerStatus.Buffering)) {
+            message.channel.send("Pause.. Pause what? Pause deez nuts.")
+        } else {
+            player.pause()
+            message.channel.send("Okay I'll pause it but you gotta let me speak too bro. You guys talk all the time and never invite me. At least let me play sometimes.")
+        }
     }
 } 
