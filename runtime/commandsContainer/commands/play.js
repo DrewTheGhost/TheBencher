3f22bdb898d6c605a40cfde891babfebe8cab843
Drew <thatonedrew22@gmail.com>
2021-10-11 11:44:11 -0500

    misc error handling that sucks ass

--- runtime/commandsContainer/commands/play.js
+++ runtime/commandsContainer/commands/play.js
@@ -37,6 +37,10 @@ module.exports = {
             searched = true
             https.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&fields=items(id,snippet(title))&maxResults=5&q=${suffix}&key=${config.youtubeAPIKey}`, (res) => {
                 let chunk = new String;
+                const { statusCode } = res;
+                if(statusCode != 200) {
+                    return message.channel.send(`Shit brokey, returned status code ${statusCode}`)
+                }
                 res.on("data", d => {
                     chunk += d.toString();
                 });
@@ -48,9 +52,11 @@ module.exports = {
                     }
                     suffix = `https://youtube.com/watch?v=${chunk.items[0].id.videoId}`
                 });
+                
+
             }).on("error", err => {
-                console.error(err);
-                message.channel.send("Can't seem to pull info from that. Did you actually put in a youtube link or proper search request, asshole?");
+                console.error(err.message);
+                return message.channel.send("Can't seem to pull info from that. Did you actually put in a youtube link or proper search request, asshole?");
             })
         }
 
@@ -60,7 +66,14 @@ module.exports = {
         */
         setTimeout(async () => {
             if(ytdl.validateURL(suffix)) {
-                requestInfo = await ytdl.getInfo(suffix)
+                let errored = false;
+                requestInfo = await ytdl.getInfo(suffix).catch(err => {
+                    errored = true;
+                    console.error(err)
+                })
+                if(errored) {
+                    return message.channel.send("SHIT BROKEY!!")
+                }
                 if(requestInfo.videoDetails.isLiveContent) {
                     return message.channel.send("Guess again if you think I'm about to let you queue a fucking livestream.")
                 }
@@ -74,8 +87,8 @@ module.exports = {
 
                     title = result.rows[0].title
                     requester = result.rows[0].requester
-                    console.debug(`${chalk.blue("Music:")}${chalk.reset()} ${chalk.yellow(result.rows[0].requester)}${chalk.reset()} Requested ${result.rows[0].title}`)
-                    console.debug(`${chalk.blue("Music:")} Song title - ${chalk.yellow(result.rows[0].title)}${chalk.reset()}\n`)
+                    console.debug(`${chalk.blue("Music:")}${chalk.reset()} ${chalk.yellow(result.rows[0].requester)}${chalk.reset()} Requested ${title}`)
+                    console.debug(`${chalk.blue("Music:")} Song title - ${chalk.yellow(title)}${chalk.reset()}\n`)
                 })
 
                 db.client.query("SELECT * FROM queue;", function(err, result) {
@@ -191,10 +204,13 @@ module.exports = {
                                                 }
                                                 stream = ytdl(result.rows[0].url, {filter: "audioonly", quality: "highestaudio"})
                                                 console.debug("Stream created using ytdl")
+
                                                 resource = createAudioResource(stream, { inputType: StreamType.Arbitrary })
                                                 console.debug("Audio resource created from stream")
+
                                                 player.play(resource)
                                                 console.debug("Player attempting to play resource")
+                                                
                                                 message.channel.send(`Now playing ${result.rows[0].title} - Requested by ${result.rows[0].requester}`)
                                             })
                                         } else {
@@ -207,7 +223,7 @@ module.exports = {
                                                     connection.destroy()
                                                     message.channel.send("Nothing left in queue, leaving!")
                                                 } catch(err) {
-                                                    console.error(err)
+                                                    return  console.error(err)
                                                 }
                                             }
                                         }
