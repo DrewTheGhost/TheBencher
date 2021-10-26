12280b25a8c4a7be066e09d50537e7e61af04a86
Drew <thatonedrew22@gmail.com>
2021-10-26 15:55:19 -0500

    temp site changes

--- runtime/commandsContainer/commands/site.js
+++ runtime/commandsContainer/commands/site.js
@@ -1,5 +1,7 @@
-let mainController = require("../mainController.json"),
-    voiceLines = mainController.voiceNames
+const { voiceLines } = require("../mainController.json"),
+    { readFile } = require("fs"),
+    { AudioPlayerStatus, StreamType, createAudioPlayer, createAudioResource, joinVoiceChannel, getVoiceConnection } = require("@discordjs/voice"),
+    { player } = require("./play.js")
     
 module.exports = {
     name: "site",
@@ -8,29 +10,27 @@ module.exports = {
     controlled: false,
     fn(message, _suffix, bot) {
         let randomVoiceLine = voiceLines[Math.floor(Math.random() * voiceLines.length)],
-            channelID = message.member.voice.channelID
+            adapter = bot.voice.adapters.get(message.guild.id) == undefined
 
-        if(channelID === null) {
+        if(!adapter) {
             return message.channel.send("You're not even in a voice channel, dumbass!")
-        } else if(bot.voice.connections.filter(m => m.channelID !== null).size > 0) {
+        } /*else if(bot.voice.connections.filter(m => m.channelID !== null).size > 0) {
             if(bot.voice.connections.filter(m => m.channelID !== null).first().speaking) {
                 return message.channel.send("I'm already playing something, fuck off")
             }
-        } else {
-            message.member.voice.channel.join().then(connection => {
-                if(connection.speaking) {
-                    connection.setSpeaking(0)
-                }
-                const dispatcher = connection.play(`./runtime/commandsContainer/voiceFiles/${randomVoiceLine}`, {volume: 1})
-                dispatcher.on("speaking", val => {
-                    if(val == 0) {
-                        connection.disconnect()
-                    }
-                })
-            }).catch(err => {
-                console.error(err)
-                return message.channel.send("Error joining voice channel or failed to play file. Dumbass code bro.")
-            })
+          */
+        else {
+            let connection = joinVoiceChannel({
+                channelId: message.member.voice.channelId,
+                guildId: message.channel.guild.id,
+                adapterCreator: message.channel.guild.voiceAdapterCreator
+            }),
+            stream = readFile(`./runtime/commandsContainer/voiceFiles/${randomVoiceLine}`),
+            resource = createAudioResource(stream, { inputType: StreamType.Arbitrary })
+
+            player.play(resource)
+            connection.subscribe(player)
+
         }
     }
 }
