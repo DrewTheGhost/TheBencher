98ba02987b68e08fe55302141476dbe1efc7fd74
Drew <thatonedrew22@gmail.com>
2021-12-22 00:22:13 -0600

    Fix skip by seeking instead

--- runtime/commandsContainer/commands/skip.js
+++ runtime/commandsContainer/commands/skip.js
@@ -4,19 +4,19 @@ module.exports = {
     description: "Skips a song.",
     controlled: false,
     fn(message, _suffix, bot, db) {
-        let player = bot.player
-        if(player == undefined) {
+        if(bot.player == undefined) {
             return message.channel.send("I'm literally not even playing anything, skip yourself asshole!")
         }
         if(message.member.voice.channelId == null) {
             return message.channel.send("You literally aren't even listening, fuck off.")
         }
-        db.client.query("DELETE FROM queue WHERE id IN (SELECT id FROM queue ORDER BY id ASC LIMIT 1) RETURNING title;", function(err, result) {
+        db.client.query("SELECT * FROM queue ORDER BY id ASC LIMIT 1;", async function(err, result) {
             if(err) {
                 console.error(err)
+                return message.channel.send("Select query errored, aborting skip.")
             }
+            bot.player.seek(`${parseInt(result.rows[0].duration)}`)
             message.channel.send(`Skipping ${result.rows[0].title}`)
         })
-        player.emit("end", {"type": "TrackEndEvent"})
     }
 } 
