5cbe64cd491c446a713b20813b69fa4c09d8c1ca
Drew <thatonedrew22@gmail.com>
2021-12-23 10:51:52 -0600

    why do I fuck with the volume again?

--- runtime/commandsContainer/commands/bassboost.js
+++ runtime/commandsContainer/commands/bassboost.js
@@ -6,7 +6,7 @@ module.exports = {
     controlled: false,
     fn(message, _suffix, bot) {
         let bands,
-            gainAmount = 0.5;
+            gainAmount = 0.5; // Potential use in the future, unsure yet
         if(bot.player == undefined) {
             return message.channel.send("Bass.. Based on what?")
         }
@@ -19,14 +19,12 @@ module.exports = {
                 bands.push({"bands": i, "gain": 0})
             }
             bot.player.equalizer(bands)
-            bot.player.volume(10)
             boosted = false;
             return message.channel.send("Unboosted. Vibes natty again.")
             
         }
         bands = [{"band": 0, "gain": 0.25}, {"band": 1, "gain": 0.25}, {"band": 2, "gain": 0.25}, {"band": 4, "band": -0.05},{"band": 5, "band": -0.05},{"band": 6, "band": -0.05},{"band": 7, "band": -0.05}]
         bot.player.equalizer(bands)
-        bot.player.volume(15)
         boosted = true;
         message.channel.send("Boosted that shit!")
     }
