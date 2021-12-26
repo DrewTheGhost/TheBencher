4b9cb8f93ef95965a2accd5f247b6c9e5f9388e2
Drew <thatonedrew22@gmail.com>
2021-12-25 19:29:20 -0600

    turn warn into a fucked up psychopath

--- runtime/commandsContainer/commands/warn.js
+++ runtime/commandsContainer/commands/warn.js
@@ -3,14 +3,16 @@ module.exports = {
     aliases: [],
     description: "Warns a domer for doing some stupid shit.",
     controlled: false,
-    fn(message, suffix) {
+    fn(message, suffix, bot) {
         suffix = suffix.split(" ")
         if(!suffix[0]) {
-            return message.channel.send(`?? Warn someone, hello?`)
+            message.channel.send(`?? Warn someone, hello?`)
+        } else if(message.mentions.users.size == 0) {
+            message.channel.send(`Dude. Ping the bastard.`)
+        } else if(message.mentions.users.has(bot.user.id)) {
+            message.channel.send("Alright.. For one, fuck you. I'm the one who issues the warnings, you pathetic little cunt. Secondly, where do you get off thinking I did something warn worthy? I'm literally the embodiment of all that is based. Go back to your little cum hole, sissy bitch.")
+        } else {
+            message.channel.send(`Alright, domer. You've done some fucked up shit. You messed up. The first step is admitting you have a problem. Own up to it, <@${message.mentions.users.first().id}>, and maybe we'll forgive you. You've been warned, shithead.`)
         }
-        if(message.mentions.users.size == 0) {
-            return message.channel.send(`Dude. Ping the bastard.`)
-        }
-        message.channel.send(`Alright, domer. You've done some fucked up shit. You messed up. The first step is admitting you have a problem. Own up to it, <@${message.mentions.users.first().id}>, and maybe we'll forgive you. You've been warned, shithead.`)
     }
 }
