3041dc6a27faae5893d450756131289e87359a7b
Drew <thatonedrew22@gmail.com>
2022-01-14 15:26:59 -0600

    this file is a fucking mess

--- runtime/commandsContainer/commands/play.js
+++ runtime/commandsContainer/commands/play.js
@@ -6,7 +6,9 @@ const chalk = require("chalk"),
         { id: "1", host: config.lavalink.host, port: config.lavalink.port, password: config.lavalink.password}
       ],
       fetch = require("node-fetch"),
-      { URLSearchParams } = require("url");
+      { URLSearchParams } = require("url"),
+      linkTest = new RegExp(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi); 
+
 
 module.exports = {
     name: "play",
@@ -14,34 +16,26 @@ module.exports = {
     description: "Plays some good shiiiiit",
     controlled: false,
     async fn(message, suffix, bot, db) {
-        let linkTest = new RegExp(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi); 
-        // Tests for links, used for determining when to search for a song and when a song is provided
-
         if(bot.manager == undefined) {
             bot.manager = new Manager(bot, nodes, { user: "801939642261307393", shards: 1 })
             await bot.manager.connect()
         }
-
         if(message.member.voice.channelId === null) {
             return message.channel.send("You aren't even in a voice channel to listen to anything, dumbass.")
         }
-
         if(suffix === "" || !suffix) {
             return message.channel.send("Actually fucking type something after play!!")
         }
-
         if(bot.manager.listenerCount("error") == 0) {
             console.debug("Creating error listener for manager.")
             bot.manager.on("error", (error, node) => {
                 console.error(`Lavalink error: ${error}\nWith node: ${node}`)
             })
         }
-
         if(!suffix.match(linkTest)) {
             suffix = "ytsearch:" + suffix
             // If there's no match for a link, we need to prepend the identifier ytsearch: to the actual search so LavaLink knows
         }
-
         await getSongs(suffix, bot).then(async results => {
             if(results.tracks.length == 0) {
                 return message.channel.send("No results found.")
@@ -50,14 +44,7 @@ module.exports = {
             // Specific handler for playlists, if a playlist is requested then I have to do a database insert for each song.
             // Looking into doing a multi-insert in one query, but need to think on it more
             if(results.loadType == "PLAYLIST_LOADED") {
-                await insertMultipleSongs(message, db, results)
-                setTimeout(() => {
-                    db.query("SELECT * FROM queue;", (err, result) => {
-                        if(err) return console.error(err)
-    
-                        message.channel.send(`${message.author.username} requested a playlist. New queue length is ${result.rows.length}.`)
-                    })
-                }, 10)
+                insertMultipleSongs(message, db, results)
             } else {
                 await insertSingleSong(message, db, results)
             }
@@ -122,19 +109,19 @@ async function playSong(message, bot, db) {
                 bot.player = undefined
             }
         })
-    }, 10)
+    }, 100)
 }
 async function dropCurrentSong(db) {
-    db.query("DELETE FROM queue WHERE id IN (SELECT id FROM queue ORDER BY id ASC LIMIT 1)", (err, _result) => {
+    db.query("DELETE FROM queue WHERE id IN (SELECT id FROM queue ORDER BY id ASC) LIMIT 1", (err, _result) => {
         if(err) return console.error(err)
     })
 }
 async function insertSingleSong(message, db, results) {
     let title = results.tracks[0].info.title, 
-    url = results.tracks[0].info.uri
-    username = message.author.username,
-    track64 = results.tracks[0].track,
-    duration = results.tracks[0].info.length;
+        url = results.tracks[0].info.uri
+        username = message.author.username,
+        track64 = results.tracks[0].track,
+        duration = results.tracks[0].info.length;
 
     db.query("SELECT * FROM queue ORDER BY id DESC;", (err, result) => {
         if(err) {
@@ -146,7 +133,6 @@ async function insertSingleSong(message, db, results) {
             id = 1
         }
     })
-    
     setTimeout(() => {
         db.query("INSERT INTO queue(title, url, requester, id, track64, duration) VALUES($1, $2, $3, $4, $5, $6) RETURNING *;", [title, url, username, id, track64, duration], function(err, result) {
             if(err) {
@@ -161,6 +147,15 @@ async function insertSingleSong(message, db, results) {
     }, 2)
 }
 async function insertMultipleSongs(message, db, results) {
+    db.query("SELECT * FROM queue ORDER BY id DESC;", (err, result) => {
+        if(err) {
+            return console.error(err)
+        } else if(result.rows.length > 0) {
+            id = result.rows[0].id + 1
+        } else if(result.rows.length == 0) {
+            id = 1
+        }
+    })
     for(const track of results.tracks) {
         if(id >= 500) {
             break
@@ -170,21 +165,19 @@ async function insertMultipleSongs(message, db, results) {
             username = message.author.username,
             track64 = track.track,
             duration = track.info.length;
-        db.query("SELECT * FROM queue ORDER BY id DESC;", (err, result) => {
-            if(err) {
-                return console.error(err)
-            } else if(result.rows.length > 0) {
-                id = result.rows[0].id + 1
-            } else if(result.rows.length == 0) {
-                id = 1
-            }
-        })
         setTimeout(() => {
             db.query("INSERT INTO queue (title, url, requester, id, track64, duration) VALUES ($1, $2, $3, $4, $5, $6);", [title, url, username, id, track64, duration], function(err, result) {
                 if(err) {
                     return console.error(err)
                 }
             })
-        }, 2)
+            id++
+        }, 1)
     }
+    setTimeout(() => {
+        db.query("SELECT * FROM queue;", (err, result) => {
+            if(err) return console.error(err)
+            message.channel.send(`${message.author.username} requested a playlist. New queue length is ${result.rows.length}.`)
+        })
+    }, 500)
 }
